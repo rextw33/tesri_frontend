9f063312698188ce88c09c3233d8c2925fbd71f2
Rex Liu <rex.liu@microtek.com.tw>
2021-07-26 09:43:21 +0800

    將 Header 固定

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,29 @@
 <template>
-  <div id="nav" class="flex justify-between">
-    <div>
-      <router-link to="/">特有生物研究保育中心植物標本館 (TAIE)</router-link>
-    </div>
-    <div class="flex">
-      <div class="px-5">
-        <a
-          href="https://test-tesri.microtekweb.com/"
-          target="_blank"
-          alt="前台管理"
-        >
-          <BriefcaseIcon class="h-6 w-6" aria-hidden="true" />
-        </a>
+  <nav class="bg-gray-300 h-16 fixed top-0 inset-x-0">
+    <div class="max-w-6xl mx-auto py-5">
+      <div class="flex justify-between">
+        <div class="flex space-x-4">
+          <router-link to="/">特有生物研究保育中心植物標本館 (TAIE)</router-link>
+        </div>
+        <div class="hidden md:flex items-center space-x-1">
+          <div class="px-5">
+            <a
+              href="https://test-tesri.microtekweb.com/"
+              target="_blank"
+              alt="前台管理"
+            >
+              <BriefcaseIcon class="h-6 w-6" aria-hidden="true" />
+            </a>
+          </div>
+          <div class="px-5">
+            <router-link to="/profile">
+              <UserCircleIcon class="h-6 w-6" aria-hidden="true" />
+            </router-link>
+          </div>
+        </div>
       </div>
-      <div class="px-5">
-        <router-link to="/profile">
-          <UserCircleIcon class="h-6 w-6" aria-hidden="true" />
-        </router-link>
-      </div>
     </div>
-  </div>
+  </nav>
   <router-view />
   <Footer />
 </template>
@@ -44,18 +48,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
